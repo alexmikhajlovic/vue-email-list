7251abccfaa6b4acd4ece1d78f6586e19e8d45a4
Alex <alexmikhajlovic@gmail.com>
2021-02-08 16:22:03 +0100

    Upgrade
    Bonus

--- js/script.js
+++ js/script.js
@@ -4,15 +4,21 @@
 var app = new Vue ({
     el: ('#app'),
     data: {
-        mails: []
+        mails: [],
+        start: false,
     },
-    mounted() {
+    created() {
         for(let i = 0; i < 10; i++){
             axios
             .get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then(result => {
                 this.mails.push(result.data.response);
             });
+        }
+    },
+    methods: {
+        deleteMail(index) {
+            this.mails.splice(index,1);
         }
     }
 });
