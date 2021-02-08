1aa0285016a032d0f4d5aabaea35c1689e5f5210
Alex <alexmikhajlovic@gmail.com>
2021-02-08 15:36:57 +0100

    Complete

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-// ? Griglia 6x6, ad ogni click parte una richiesta AJAX che prende un numero random da 1 a 9.
-// ? Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
-// ? Il numero ottenuto appare al centro del quadrato
+// ? Genera 10 mail tramite api e stampale in una lista.
+// ? Generate 10 emails via api and print them in a list.
 
 var app = new Vue ({
     el: ('#app'),
